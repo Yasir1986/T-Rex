f6e17f40e193bfcb483d2c7f35dbad4809be5b2b
Yasir Akbar <yasir.akbar86@hotmail.com>
2020-07-07 15:45:41 +0100

    Jump fun added

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const dino = document.querySelector('.dino')
+document.addEventListener("DOMContentLoaded", () => {
+  const dino = document.querySelector(".dino");
+  let isJumping = false;
+  let gravity = 0.9;
 
-    function control(e) {
-        if (e.keyCode === 32) {
-            console.log('pressed')
-        }   
+  function control(e) {
+    if (e.keyCode === 32) {
+      console.log("pressed");
+      if (isJumping === false) {
+        isJumping = true;
+        jump();
+      }
     }
-    document.addEventListener('keyup', control)
-})
+  }
+
+  document.addEventListener("keyup", control);
+
+  let position = 0;
+  function jump() {
+    let count = 0;
+    let timerId = setInterval(function () {
+      //move down
+
+      if (count === 15) {
+        clearInterval(timerId);
+        console.log("down");
+        let downTimerId = setInterval(function () {
+          if (count === 0) {
+            clearInterval(downTimerId);
+            isJumping = false;
+          }
+          position -= 10;
+          count--;
+          position = position * gravity;
+          dino.style.bottom = position + "px";
+        }, 20);
+      }
+
+      //move up
+      console.log("up");
+      position += 30;
+      count++;
+      position = position + gravity;
+      dino.style.bottom = position + "px";
+      console.log(dino.style.bottom);
+    }, 20);
+  }
+
+  function generateObstacles() {
+      
+  }
+});
