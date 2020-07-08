5aea549e443f10f45fd3c2355805b4de2231e945
Yasir Akbar <yasir.akbar86@hotmail.com>
2020-07-08 13:57:59 +0100

    Game Functionality completed

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const dino = document.querySelector(".dino");
   const grid = document.querySelector(".grid");
+  const alert = document.getElementById("alert");
   let isJumping = false;
   let gravity = 0.9;
+  let isGameOver = false;
 
   function control(e) {
     if (e.keyCode === 32) {
@@ -48,20 +50,27 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function generateObstacles() {
+    let randomTime = Math.random() * 4000;
     let obstaclePosition = 1000;
     const obstacle = document.createElement("div");
-    obstacle.classList.add("obstacle");
+    if (!isGameOver) obstacle.classList.add("obstacle");
     grid.appendChild(obstacle);
     obstacle.style.left = obstaclePosition + "px";
 
     let timerId = setInterval(function () {
-      if (obstaclePosition === 0) {
+      if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
         clearInterval(timerId);
-        alert("GAME OVER!");
+        alert.innerHTML = "Game Over";
+        isGameOver = true;
+        //remove all moving obstacles
+        while (grid.firstChild) {
+          grid.removeChild(grid.lastChild)
+        }
       }
       obstaclePosition -= 10;
       obstacle.style.left = obstaclePosition + "px";
     }, 20);
+    if (!isGameOver) setTimeout(generateObstacles, randomTime);
   }
   generateObstacles();
 });
