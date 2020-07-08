95006e9cb4274f48bc5e42523da955e4523cb401
Yasir Akbar <yasir.akbar86@hotmail.com>
2020-07-08 15:06:15 +0100

    Images added

--- app.js
+++ app.js
@@ -1,76 +1,73 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const dino = document.querySelector(".dino");
-  const grid = document.querySelector(".grid");
-  const alert = document.getElementById("alert");
-  let isJumping = false;
-  let gravity = 0.9;
-  let isGameOver = false;
-
+document.addEventListener('DOMContentLoaded', () => {
+  const dino = document.querySelector('.dino')
+  const grid = document.querySelector('.grid')
+  const body = document.querySelector('body')
+  const alert = document.getElementById('alert')
+  let isJumping = false
+  let gravity = 0.9
+  let isGameOver = false
+  
   function control(e) {
     if (e.keyCode === 32) {
-      console.log("pressed");
-      if (isJumping === false) {
-        isJumping = true;
-        jump();
+      if (!isJumping) {
+        isJumping = true
+        jump()
       }
     }
   }
-
-  document.addEventListener("keyup", control);
-
-  let position = 0;
+  document.addEventListener('keyup', control)
+  
+  let position = 0
   function jump() {
-    let count = 0;
+    let count = 0
     let timerId = setInterval(function () {
       //move down
-
       if (count === 15) {
-        clearInterval(timerId);
-        console.log("down");
+        clearInterval(timerId)
         let downTimerId = setInterval(function () {
           if (count === 0) {
-            clearInterval(downTimerId);
-            isJumping = false;
+            clearInterval(downTimerId)
+            isJumping = false
           }
-          position -= 10;
-          count--;
-          position = position * gravity;
-          dino.style.bottom = position + "px";
-        }, 20);
+          position -= 5
+          count--
+          position = position * gravity
+          dino.style.bottom = position + 'px'
+        },20)
+  
       }
-
       //move up
-      console.log("up");
-      position += 30;
-      count++;
-      position = position + gravity;
-      dino.style.bottom = position + "px";
-      console.log(dino.style.bottom);
-    }, 20);
+      position +=30
+      count++
+      position = position * gravity
+      dino.style.bottom = position + 'px'
+    },20)
   }
-
+  
   function generateObstacles() {
-    let randomTime = Math.random() * 4000;
-    let obstaclePosition = 1000;
-    const obstacle = document.createElement("div");
-    if (!isGameOver) obstacle.classList.add("obstacle");
-    grid.appendChild(obstacle);
-    obstacle.style.left = obstaclePosition + "px";
-
-    let timerId = setInterval(function () {
+    let randomTime = Math.random() * 4000
+    let obstaclePosition = 1000
+    const obstacle = document.createElement('div')
+    if (!isGameOver) obstacle.classList.add('obstacle')
+    grid.appendChild(obstacle)
+    obstacle.style.left = obstaclePosition + 'px'
+  
+    let timerId = setInterval(function() {
       if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
-        clearInterval(timerId);
-        alert.innerHTML = "Game Over";
-        isGameOver = true;
-        //remove all moving obstacles
+        clearInterval(timerId)
+        alert.innerHTML = 'Game Over'
+        isGameOver = true
+        //remove all children
+        body.removeChild(body.firstChild)
         while (grid.firstChild) {
           grid.removeChild(grid.lastChild)
         }
+        
       }
-      obstaclePosition -= 10;
-      obstacle.style.left = obstaclePosition + "px";
-    }, 20);
-    if (!isGameOver) setTimeout(generateObstacles, randomTime);
+      obstaclePosition -=10
+      obstacle.style.left = obstaclePosition + 'px'
+    },20)
+    if (!isGameOver) setTimeout(generateObstacles, randomTime)
   }
-  generateObstacles();
-});
+  generateObstacles()
+  })
